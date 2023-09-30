c0ff5bf66bbcda6435c73674cb18e79e07bffc40
George <117456377+georgetechstat@users.noreply.github.com>
2023-10-01 00:39:30 +0400

    (fully working) monkeytype autotype script

---
+++ pyprojects/V3.0-monkeytype.py
@@ -0,0 +1,40 @@
+from helium import start_chrome
+from bs4 import BeautifulSoup
+from pynput.keyboard import Key, Controller
+import time
+
+url = "https://monkeytype.com/"
+
+browser = start_chrome(url, headless=False) # headless = True if you want the browser window to NOT appear
+soup = BeautifulSoup(browser.page_source, "html.parser")
+kb = Controller()
+time.sleep(2) # Let the browser fully load dynamic DOM elements
+
+word_element = soup.find_all(attrs={"class":"word"})
+
+def extract_words(words) -> list[str]:
+    query = ""
+    for word in words:
+        current_letters = word.find_all("letter")
+
+        for letter in current_letters:
+            query += letter.text
+        query += " "
+    return query.split(" ")[:-1] # [:-1] simply doesn't include the empty string at the end of the list
+
+def write_text(txt: list[str]) -> None:
+    start = time.time()
+    for word in txt:
+        if time.time() - start >= 30:
+            break
+
+        for letter in word:
+            kb.press(letter)
+            time.sleep(0.1)
+            kb.release(letter)
+        kb.press(Key.space)
+        time.sleep(0.1)
+        kb.release(Key.space)
+
+time.sleep(2)
+write_text(extract_words(word_element))
