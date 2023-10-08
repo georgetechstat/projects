3132cc2539406d68c05463c8fc4af2c4260e572a
George <117456377+georgetechstat@users.noreply.github.com>
2023-10-08 16:28:54 +0400

    Monkeytype autotype script complete

--- pyprojects/V3.0-monkeytype.py
+++ pyprojects/V3.0-monkeytype.py
@@ -4,38 +4,57 @@ from pynput.keyboard import Key, Controller
 import time
 
 url = "https://monkeytype.com/"
-
 browser = start_chrome(url, headless=False) # headless = True if you want the browser window to NOT appear
-soup = BeautifulSoup(browser.page_source, "html.parser")
 kb = Controller()
 time.sleep(2) # Let the browser fully load dynamic DOM elements
+
+# You can set these properties
+# KEYDELAY and total_time in seconds
 KEYDELAY: float = 0.05
+total_time: float | int = 30
 
-word_element = soup.find_all(attrs={"class":"word"})
+def get_words():
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    breakpoint_word = soup.find(attrs={"class", "word active"})
+    words = breakpoint_word.find_all_next(attrs={"class", "word"})
 
-def extract_words(words) -> list[str]:
     query = ""
+
+    for letter in breakpoint_word.find_all("letter"):
+        query += letter.text
+    query += " "
+
     for word in words:
         current_letters = word.find_all("letter")
-
         for letter in current_letters:
             query += letter.text
         query += " "
+    
     return query.split(" ")[:-1] # [:-1] simply doesn't include the empty string at the end of the list
 
-def write_text(txt: list[str]) -> None:
-    start = time.time()
-    for word in txt:
-        if time.time() - start >= 30:
-            break
-
-        for letter in word:
-            kb.press(letter)
-            time.sleep(KEYDELAY)
-            kb.release(letter)
-        kb.press(Key.space)
+def type_word(word) -> None:
+    for letter in word:
+        kb.press(letter)
         time.sleep(KEYDELAY)
-        kb.release(Key.space)
+        kb.release(letter)
+    kb.press(Key.space)
+    time.sleep(KEYDELAY)
+    kb.release(Key.space)
+
+def timeout(st):
+    return time.time() - st <= total_time
+
+idx = 0
+current = get_words()
 
 time.sleep(2)
-write_text(extract_words(word_element))
+start_time = time.time()
+
+while (timeout(start_time)):
+    if idx == current.__len__() - 1:
+        type_word(current[idx])
+        current = get_words()
+        idx = 0
+
+    type_word(current[idx])
+    idx += 1
