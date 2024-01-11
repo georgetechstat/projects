9cdc4dfa63b5db78592429dba8955303fc5c6418
George <117456377+georgetechstat@users.noreply.github.com>
2024-01-11 21:02:28 +0400

    refactoring code

--- pyprojects/shooter/tile_editor.py
+++ pyprojects/shooter/tile_editor.py
@@ -22,14 +22,34 @@ tile.fill(tile_color)
 # A rect which is created only once, isn't copied and shared among all tiles
 rct = pygame.rect.Rect(0, 0, tile_size, tile_size)
 
+def draw_rows():
+    global screen, height, tile_size
+    for r in range(height // tile_size):
+        pygame.draw.line(screen, (0, 255, 0), (0, r * tile_size), (width, r * tile_size))
+
+def draw_cols():
+    global screen, width, tile_size
+    for c in range(width // tile_size):
+        pygame.draw.line(screen, (0, 255, 0), (c * tile_size, 0), (c * tile_size, height))
+
+def draw_rects():
+    global TILE_TOPLEFT_POS, rct, screen
+    for pos in TILE_TOPLEFT_POS:
+        rct.topleft = pos
+        screen.blit(tile, rct)
+
+def quit_map():
+    global TILEMAP, TILE_TOPLEFT_POS
+    TILEMAP["tile_pos"] = list(TILE_TOPLEFT_POS)
+    with open(os.path.join(os.getcwd(), "shooter/map.json"), "w") as f:
+        f.write(json.dumps(TILEMAP))
+
 while True:
     screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            TILEMAP["tile_pos"] = list(TILE_TOPLEFT_POS)
-            with open(os.path.join(os.getcwd(), "shooter/map.json"), "w") as f:
-                f.write(json.dumps(TILEMAP))
+            quit_map()
             exit()
         
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -39,15 +59,9 @@ while True:
                 r = (my // tile_size) * tile_size
                 TILE_TOPLEFT_POS.add((c, r))
 
-    for c in range(width // tile_size):
-        pygame.draw.line(screen, (0, 255, 0), (c * tile_size, 0), (c * tile_size, height))
-    
-    for r in range(height // tile_size):
-        pygame.draw.line(screen, (0, 255, 0), (0, r * tile_size), (width, r * tile_size))
-
-    for pos in TILE_TOPLEFT_POS:
-        rct.topleft = pos
-        screen.blit(tile, rct)
+    draw_cols()
+    draw_rows()
+    draw_rects()
 
     pygame.display.update()
     clock.tick(60)
